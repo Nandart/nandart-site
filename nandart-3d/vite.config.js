79cdbb3d90c9a49a0f25d819267afd57c4e0744b
Nandart <reguinenu@gmail.com>
2025-04-30 04:39:00 +0100

    Update vite.config.js

--- nandart-3d/vite.config.js
+++ nandart-3d/vite.config.js
@@ -1,10 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: '.',
-  publicDir: 'public',
-  build: {
-    outDir: 'dist'
-  }
+  base: '/nandart-site/nandart-3d/', // <-- caminho completo do GitHub Pages
 });
-
