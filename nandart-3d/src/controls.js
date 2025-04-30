57970974da40fd6f8f61d7deeaee05532ada161b
Nandart <reguinenu@gmail.com>
2025-04-30 02:54:18 +0100

    Update controls.js

--- nandart-3d/src/controls.js
+++ nandart-3d/src/controls.js
@@ -1,1 +1,11 @@
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+export function setupControls(camera, domElement) {
+  const controls = new OrbitControls(camera, domElement);
+  controls.enableDamping = true;
+  controls.dampingFactor = 0.05;
+  controls.rotateSpeed = 0.5;
+  controls.zoomSpeed = 1.2;
+  controls.panSpeed = 0.8;
+}
 
