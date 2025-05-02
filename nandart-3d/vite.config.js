7b01ee159f7328c57a0ff392b32f4e1f2c8df72c
Nandart <reguinenu@gmail.com>
2025-05-02 20:09:27 +0100

    Update vite.config.js

--- nandart-3d/vite.config.js
+++ nandart-3d/vite.config.js
@@ -1,1 +1,19 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  server: {
+    host: true
+  }
+});
 
