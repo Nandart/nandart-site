7a8d823b78c350208bb378ffe1ec9e2c3a661c3d
Nandart <reguinenu@gmail.com>
2025-05-04 16:18:46 +0100

    Update vite.config.js

--- nandart-3d/vite.config.js
+++ nandart-3d/vite.config.js
@@ -1,1 +1,8 @@
-
+// vite.config.js
+export default {
+  root: '.',              // Usa a raiz do projeto
+  build: {
+    outDir: 'dist',       // Onde o Vercel vai procurar os ficheiros gerados
+    emptyOutDir: true
+  }
+}
