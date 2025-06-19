0cf9d377d49ffc78a7ce5770921221c1f425c348
Nandart <reguinenu@gmail.com>
2025-06-19 13:53:43 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,11 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 
 export default defineConfig({
   base: './',
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src')
-    }
-  },
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        info: resolve(__dirname, 'info.html')
-      }
-    }
+    outDir: 'dist'
   },
   server: {
     open: true
   }
 });
-
